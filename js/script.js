9b8eb9ec3ca09d3c7a450f38c4fdd2bae285a26c
Ghada-M-Elsanhory <110494028+Ghada-M-Elsanhory@users.noreply.github.com>
2024-09-19 07:05:19 +0300

    gallery

--- js/script.js
+++ js/script.js
@@ -1,30 +1,19 @@
-// // script.js
+// // function toggleMenu() {
+// //     var navMenu = document.getElementById('nav-menu');
+// //     if (navMenu.classList.contains('active')) {
+// //         navMenu.classList.remove('active');
+// //     } else {
+// //         navMenu.classList.add('active');
+// //     }
+// // }
 
-// // Function to toggle the navigation menu and overlay visibility
+
 // function toggleMenu() {
 //     const navMenu = document.getElementById('nav-menu');
-//     const overlay = document.querySelector('.overlay');
-    
 //     navMenu.classList.toggle('active');
-//     overlay.classList.toggle('active');
 // }
-
-// // Function to redirect to the "Projects" page
-// function redirectToContact() {
-//     window.location.href = 'projects.html';
-// }
-
-// // Example JavaScript for interactive circles (if needed)
-// // Function to handle click event on circles
-// document.querySelectorAll('.circle').forEach(circle => {
-//     circle.addEventListener('click', () => {
-//         alert('Circle clicked!');
-//     });
-// });
 
 // script.js
-
-// Function to toggle the navigation menu and overlay visibility
 function toggleMenu() {
     const navMenu = document.getElementById('nav-menu');
     const overlay = document.querySelector('.overlay');
@@ -32,17 +21,8 @@ function toggleMenu() {
     navMenu.classList.toggle('active');
     overlay.classList.toggle('active');
 }
-
-// Function to redirect to the "Projects" page
+// script.js
 function redirectToProjects() {
     window.location.href = 'projects.html';
-}
-
-// Example JavaScript for interactive circles (if needed)
-// Function to handle click event on circles
-document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', () => {
-        const textContent = card.nextElementSibling;
-        textContent.classList.toggle('active');
-    });
-});
+  }
+  
