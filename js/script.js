f8dc69319fb305b73fbdeb29437e90462bd48ebe
Ghada-M-Elsanhory <110494028+Ghada-M-Elsanhory@users.noreply.github.com>
2024-09-19 03:50:50 +0300

    projects

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
+// // function toggleMenu() {
+// //     var navMenu = document.getElementById('nav-menu');
+// //     if (navMenu.classList.contains('active')) {
+// //         navMenu.classList.remove('active');
+// //     } else {
+// //         navMenu.classList.add('active');
+// //     }
+// // }
+
+
+// function toggleMenu() {
+//     const navMenu = document.getElementById('nav-menu');
+//     navMenu.classList.toggle('active');
+// }
+
+// script.js
 function toggleMenu() {
-    var navMenu = document.getElementById('nav-menu');
-    if (navMenu.classList.contains('active')) {
-        navMenu.classList.remove('active');
-    } else {
-        navMenu.classList.add('active');
-    }
+    const navMenu = document.getElementById('nav-menu');
+    const overlay = document.querySelector('.overlay');
+    
+    navMenu.classList.toggle('active');
+    overlay.classList.toggle('active');
 }
+// script.js
+function redirectToContact() {
+    window.location.href = 'projects.html';
+  }
+  
