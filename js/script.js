780dd59772ea75ea8e2f17c07b2841f43f684acc
Ghada-M-Elsanhory <110494028+Ghada-M-Elsanhory@users.noreply.github.com>
2024-09-22 07:10:32 +0300

    contact

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-// // function toggleMenu() {
-// //     var navMenu = document.getElementById('nav-menu');
-// //     if (navMenu.classList.contains('active')) {
-// //         navMenu.classList.remove('active');
-// //     } else {
-// //         navMenu.classList.add('active');
-// //     }
-// // }
-
-
-// function toggleMenu() {
-//     const navMenu = document.getElementById('nav-menu');
-//     navMenu.classList.toggle('active');
-// }
 
 // script.js
 function toggleMenu() {
